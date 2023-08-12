7949d8ca35cb8cac351d73a5d29a9bbe8ce2b6e0
Abdulaziz <orazbaev.abdulaziz.98@gmail.com>
2023-08-12 11:01:40 +0500

    Sidebar component has been created

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
-import Header from '../components/common/Header.vue';
+import Header from '../components/common/Header.vue'
+import Sidebar from '../components/common/Sidebar.vue'
 </script>
 
 <template>
   <Header></Header>
+  <Sidebar></Sidebar>
   <main>
   </main>
 </template>
