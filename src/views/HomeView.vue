e711c72753b1bc4622c29e3bbd7070dd72167ff7
Abdulaziz <orazbaev.abdulaziz.98@gmail.com>
2023-08-12 11:21:49 +0500

    Footer component has been created

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,17 @@
 <script setup lang="ts">
 import Header from '../components/common/Header.vue'
 import Sidebar from '../components/common/Sidebar.vue'
+import Footer from '../components/common/Footer.vue'
 </script>
 
 <template>
   <Header></Header>
   <Sidebar></Sidebar>
-  <main>
-  </main>
+  <div class="p-2 sm:ml-64">
+      <div class="p-4 mt-14">
+        <main>
+        </main>
+        <Footer></Footer>
+      </div>
+  </div>
 </template>
