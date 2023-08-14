29fbd4b7d36ec461476c2974337e2d0a67deedb4
“javohirtohirovich” <javoerg0330@gmail.com>
2023-08-14 23:00:16 +0500

    Dark mode, LoginView

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,13 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
+import FlowbiteSetup from './FlowbiteSetup.vue';
+
 </script>
 
 <template>
 
   <RouterView />
+  <FlowbiteSetup></FlowbiteSetup>
 </template>
 
 <style scoped>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import LoginView from '../views/auth/LoginView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -16,7 +17,13 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue')
+    },
+    {
+      path:'/auth/login',
+      name: 'login',
+      component: LoginView
     }
+    
   ]
 })
 
