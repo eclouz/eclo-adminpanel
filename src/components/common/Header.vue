6749dbeec02b2cfdcf49064b4f1aa5eae444c046
Abdulaziz <orazbaev.abdulaziz.98@gmail.com>
2023-08-14 21:58:23 +0500

    Sign In page has been created

--- src/components/common/Header.vue
+++ src/components/common/Header.vue
@@ -18,8 +18,8 @@ import ThemeSwitcher from './ThemeSwitcher.vue';
                             </path>
                         </svg>
                     </button>
-                    <a href="https://flowbite.com" class="flex ml-2 md:mr-24">
-                        <img src="https://flowbite.com/docs/images/logo.svg" class="h-8 mr-3" alt="FlowBite Logo" />
+                    <a class="flex ml-2 md:mr-24">
+                        <img src="../../assets/logo.svg" class="h-8 mr-3" alt="FlowBite Logo" />
                         <span
                             class="self-center text-xl font-semibold sm:text-2xl whitespace-nowrap dark:text-white">Eclo</span>
                     </a>
