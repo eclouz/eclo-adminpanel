77883e2f9809f6d714935fcf25557aa3ae27fd8d
“javohirtohirovich” <javoerg0330@gmail.com>
2023-08-23 01:31:02 +0500

    Fix Edit Delete

--- src/components/categories/CategoryEditComponent.vue
+++ src/components/categories/CategoryEditComponent.vue
@@ -1,34 +1,66 @@
 <script lang="ts">
 import { defineComponent } from "vue";
-import IconEdit from "../icons/common/IconEdit.vue"
+import IconEdit from "../icons/common/IconEdit.vue";
+import axios from '@/plugins/axios';
+
 export default defineComponent({
     components: {
         IconEdit
     },
-    props:{
-        isShow:Boolean
+    props: {
+         nameProp:String,
+         descriptionProp:String,
+         idProp:Number
     },
-    mounted() {
-        
+    data() {
+        return {
+            showModal: false,
+            categoryName: "",
+            categoryDescription: "",
+        };
     },
+    methods: {
+        openModal() {
+            this.categoryName = this.nameProp!; 
+            this.categoryDescription = this.descriptionProp!;
+            this.showModal = true;
+        },
+        closeModal() {
+            this.showModal = false;
+        },
+        async submitForm() {
+            // Bu funksiya tahrirlangan ma'lumotlarni yuborish uchun ishlatiladi
+            // Ma'lumotlarni this.categoryName va this.categoryDescription orqali olishingiz mumkin
+            // Yuborish logikasini qo'shishingiz kerak
+            const response = await axios.put("/api/admin/categories/"+this.idProp, { "name": this.categoryName, "description": this.categoryDescription }, {
+                headers: {
+                    "Content-Type": "multipart/form-data",
+                },
+            });
+        },
+    },
+
+
+
 });
 </script>
+
 <template>
-     <!--begin:: Edit Modal Button-->
-     
-    <button data-modal-target="edit-modal" data-modal-toggle="edit-modal" href="#"
+    <!--begin:: Edit Modal Button-->
+
+    <button @click="openModal"
         class="font-medium text-blue-600 dark:text-blue-500 hover:underline">
         <IconEdit> </IconEdit>
     </button>
-     <!--end:: Edit Modal Button-->
+    <!--end:: Edit Modal Button-->
 
     <!--For Edit Modal Window  Start-->
-    <div id="edit-modal" tabindex="-1" aria-hidden="true"
-        class="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
+    <div v-if="showModal"
+        class="fixed top-0 left-0 right-0 z-50 w-full h-screen flex items-center justify-center bg-black bg-opacity-50">
         <div class="relative w-full max-w-md max-h-full">
             <!-- Modal content -->
             <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
-                <button type="button"
+                <button @click="closeModal"
                     class="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
                     data-modal-hide="edit-modal">
                     <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
@@ -41,13 +73,13 @@ export default defineComponent({
                 <div class="px-6 py-6 lg:px-8">
                     <h3 class="mb-4 text-xl font-medium text-gray-900 dark:text-white">Edit
                         Category</h3>
-                    <form class="space-y-6" action="#">
+                    <form @submit.prevent="submitForm" class="space-y-6" action="#">
                         <!--Category Name Edit Start-->
                         <div>
                             <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Category
                                 name
                             </label>
-                            <input type="text" name="name" id="name"
+                            <input v-model="categoryName" type="text" name="name" id="name"
                                 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
                                 required>
                         </div>
@@ -58,10 +90,10 @@ export default defineComponent({
                             <label for="description"
                                 class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Category
                                 description</label>
-                            <textarea type="text" name="description" id="description"
+                            <textarea v-model="categoryDescription" name="description" id="description"
                                 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
                                 required>
-                                                </textarea>
+                            </textarea>
                         </div>
                         <!--Edit Description End-->
                         <div class="m-5">
