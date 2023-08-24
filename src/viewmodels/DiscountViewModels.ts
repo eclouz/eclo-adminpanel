c66754747a06aedd86512a44fba0f449f1f7b0f9
Abdulaziz <orazbaev.abdulaziz.98@gmail.com>
2023-08-24 23:35:55 +0500

    Discounts

---
+++ src/viewmodels/DiscountViewModels.ts
@@ -0,0 +1,8 @@
+export class DiscountViewModel {
+    id: Number = 0;
+    name: string = "";
+    percentage: Number = 0;
+    description: string = "";
+    createdAt: Date = new Date();
+    updatedAt: Date = new Date();
+}
