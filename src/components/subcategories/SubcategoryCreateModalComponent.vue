2624ac9e66e4eae72ae17fc27300fb825bd7ebb8
Abdulaziz <orazbaev.abdulaziz.98@gmail.com>
2023-08-25 09:04:11 +0500

    Category and Subcategory's search have been done

--- src/components/subcategories/SubcategoryCreateModalComponent.vue
+++ src/components/subcategories/SubcategoryCreateModalComponent.vue
@@ -56,7 +56,7 @@ export default defineComponent({
     <!-- Modal toggle -->
     <div class="grid justify-items-end ...">
         <button @click="selectAsync" data-modal-target="create-modal" data-modal-toggle="create-modal"
-            class="m-3 block text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
+            class="m-3 block text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-full text-sm px-2 py-2 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
             type="button">
             <IconCreate></IconCreate>
         </button>
--- src/views/categories/CategoriesView.vue
+++ src/views/categories/CategoriesView.vue
@@ -26,6 +26,19 @@ export default defineComponent({
             var response = await axios.get<CategoryViewModel[]>("/api/common/categories?page=1");
             this.isLoaded = true;
             this.categoriesList = response.data;
+        },
+        performSearch() {
+            const query = this.searchQuery.toLowerCase();
+            if (query === '') {
+                return this.categoriesList;
+            }
+            const filteredItems = this.categoriesList.filter(category =>
+                category.name.toLowerCase().includes(query)
+            );
+            return filteredItems;
+        },
+        searchOnEnter() {
+            this.performSearch();
         }
 
     },
@@ -34,10 +47,19 @@ export default defineComponent({
     },
     data() {
         return {
+            searchQuery: '',
             categoriesList: [] as CategoryViewModel[],
             defaultSkeletons: 1 as Number,
             isLoaded: false as Boolean
         }
+    },
+    computed: {
+        filteredItems() {
+        const query = this.searchQuery.toLowerCase();
+            return this.categoriesList.filter(category =>
+                category.name.toLowerCase().includes(query)
+            );
+        },
     },
     async mounted() {
         await this.getDataAsync();
@@ -61,13 +83,28 @@ export default defineComponent({
     <!--end:: Categories Skeletons-->
 
     <div v-show="isLoaded == true">
-
-        <!--begin:: Create Modal-->
-        <CategoryCreatModalComponents></CategoryCreatModalComponents>
-        <!--end:: Create Modal-->
+        <div class="flex justify-between items-center">
+            <!-- begin:: SearchPanel -->
+            <div class="flex items-center justify-end py-4 bg-white dark:bg-gray-800">
+                <label for="table-search" class="sr-only">Search</label>
+                <div class="relative">
+                    <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                        <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+                        </svg>
+                    </div>
+                    <input @keyup.enter="searchOnEnter" v-model="searchQuery" @input="performSearch" type="text" class="block p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search for users">
+                </div>
+            </div>
+            <!-- end:: SearchPanel -->
+            
+            <!--begin:: Create Modal-->
+            <CategoryCreatModalComponents></CategoryCreatModalComponents>
+            <!--end:: Create Modal-->
+        </div>
 
         <!--begin:: Table-->
-        <div class="mt-4 relative overflow-x-auto shadow-md sm:rounded-lg">
+        <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
             <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
 
                 <!--begin:: Table Head-->
@@ -76,7 +113,7 @@ export default defineComponent({
 
                 <!--Table Body Start-->
                 <tbody>
-                    <template v-for="element in categoriesList">
+                    <template v-for="element in filteredItems">
                         <CategoryTabelBodyComponents :id=element.id :name=element.name :description=element.description
                             :createdAt=element.createdAt :updatedAt=element.updatedAt>
                         </CategoryTabelBodyComponents>
